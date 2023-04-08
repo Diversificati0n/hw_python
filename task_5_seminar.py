5e72b5896b8e99ddf1b0fe2958cc15ae3a1dc45f
Diversification <startsevprm@gmail.com>
2023-04-08 15:01:24 +0500

    add

--- task_5_seminar.py
+++ task_5_seminar.py
@@ -4,19 +4,19 @@ Sample Output [ ["ate", "eat", "tea"],
 ["nat", "tan"], ["bat"] ] Т . е . 
 сгруппировать слова по " общим буквам ".'''
 
-def group_letter(input_list):
-    word_dict = {}
-    for word in input_list:
-        if (frozenset(word), len(word)) not in word_dict:
-            word_dict[(frozenset(word), len(word))] = [word]
-        else:
-            word_dict[(frozenset(word), len(word))].append(word)
-    res_list = []
-    for value in word_dict.values():
-        res_list.append(value)
-    return res_list
+# def group_letter(input_list):
+#     word_dict = {}
+#     for word in input_list:
+#         if (frozenset(word), len(word)) not in word_dict:
+#             word_dict[(frozenset(word), len(word))] = [word]
+#         else:
+#             word_dict[(frozenset(word), len(word))].append(word)
+#     res_list = []
+#     for value in word_dict.values():
+#         res_list.append(value)
+#     return res_list
 
-print(group_letter(["eat", "tea", "tan", "ate", "nat", "bat", 'batt']))
+# print(group_letter(["eat", "tea", "tan", "ate", "nat", "bat", 'batt']))
 
 
 '''Дана строка ( возможно , пустая ), состоящая из букв A-Z: AAAABBBCCXYZDDDDEEEFFFAAAAAABBBB BBBBBBBBBBBBBBBBBBBBBBBB 
@@ -25,21 +25,21 @@ print(group_letter(["eat", "tea", "tan", "ate", "nat", "bat", 'batt']))
 Если символ повторяется более 1 раза , к нему добавляется количество повторений .'''
 
 
-# def rle(some_str):
-#     res_list = []
-#     some_str += ' '
-#     temp_letter = some_str[0]
-#     count_letter = 1
-#     for ind in range(1, len(some_str)):
-#         if some_str[ind] == temp_letter:
-#             count_letter += 1
-#         else:
-#             if count_letter == 1:
-#                 res_list.append(f'{temp_letter}')
-#             else:
-#                 res_list.append(f'{temp_letter}{count_letter}')
-#             count_letter = 1
-#             temp_letter = some_str[ind]
-#     print(res_list)
-#     print(*res_list, sep='')
-# rle('AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB')
+def rle(some_str):
+    res_list = []
+    some_str += ' '
+    temp_letter = some_str[0]
+    count_letter = 1
+    for ind in range(1, len(some_str)):
+        if some_str[ind] == temp_letter:
+            count_letter += 1
+        else:
+            if count_letter == 1:
+                res_list.append(f'{temp_letter}')
+            else:
+                res_list.append(f'{temp_letter}{count_letter}')
+            count_letter = 1
+            temp_letter = some_str[ind]
+    print(res_list)
+    print(*res_list, sep='')
+rle('AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB')
